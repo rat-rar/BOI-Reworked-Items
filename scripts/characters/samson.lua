6797331d7b39bd864b3ffe80da91aaab1c7d3406
Swagpillar <kfeeley20@gmail.com>
2025-04-11 20:11:24 +0100

    let me cook now

--- scripts/characters/samson.lua
+++ scripts/characters/samson.lua
@@ -3,6 +3,8 @@ local mod = ISAACREBALANCED
 ---@param player EntityPlayer
 function mod:SamsonInitLevelStats(player)
     player:AddCollectible(CollectibleType.COLLECTIBLE_BLOOD_RIGHTS)
+    player:TryRemoveTrinket(TrinketType.TRINKET_CHILDS_HEART)
+    player:AddSmeltedTrinket(TrinketType.TRINKET_CHILDS_HEART)
 end
 
 mod:AddCallback(ModCallbacks.MC_PLAYER_INIT_POST_LEVEL_INIT_STATS, mod.SamsonInitLevelStats, PlayerType.PLAYER_SAMSON)
